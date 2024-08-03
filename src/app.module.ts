3f52bc11b0f348fa2e2957228b29736abd89c56e
ortima <account.net@mail.ru>
2024-08-03 16:37:56 +0300

    prisma init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, CategoryModule, TransactionModule, UserModule],
+  imports: [
+    AuthModule,
+    CategoryModule,
+    TransactionModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
